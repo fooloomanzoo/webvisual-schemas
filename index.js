2df1544324c2fb9b6a950bf3e0920c15e9ecdccc
fooloomanzoo <hannes-b@web.de>
2017-08-28 15:51:01 +0200

    small fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   server: require('./lib/server.json'),
   database: require('./lib/database.json'),
-  configfiles: require('./lib/configfiles.json'),
-  configfile: require('./lib/configfile.json')
+  configfiles: require('./lib/configfiles.json')
 }
